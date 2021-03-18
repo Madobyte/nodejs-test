7440f9f272d0b4cb536b5e320d19e1749e63b6dc
madobyte <madobyte@gmail.com>
2021-03-18 18:48:07 +0800

    Added a pages folder

--- index.js
+++ index.js
@@ -2,19 +2,25 @@ const http = require("http");
 const fs = require("fs");
 const url = require("url");
 
-http
-  .createServer((req, res) => {
-    const q = url.parse(req.url, true);
-    const filename = q.pathname === "/" ? "index.html" : `.${q.pathname}.html`;
+const hostname = "127.0.0.1";
+const port = 8080;
 
-    fs.readFile(filename, (err, data) => {
-      if (err) {
-        res.writeHead(302, { Location: "/404" });
-        return res.end();
-      }
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(data);
+const server = http.createServer((req, res) => {
+  const q = url.parse(req.url, true);
+  const filename =
+    q.pathname === "/" ? "pages/index.html" : `pages/${q.pathname}.html`;
+
+  fs.readFile(filename, (err, data) => {
+    if (err) {
+      res.writeHead(302, { Location: "/404" });
       return res.end();
-    });
-  })
-  .listen(8080);
+    }
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(data);
+    return res.end();
+  });
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}`);
+});
