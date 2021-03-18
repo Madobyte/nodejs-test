7d7af71fcdd593de7724848d02779372a25e686f
madobyte <madobyte@gmail.com>
2021-03-18 17:34:18 +0800

    Removed .html file extension in URLs

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ const url = require("url");
 http
   .createServer((req, res) => {
     const q = url.parse(req.url, true);
-    const filename = "." + q.pathname;
+    const filename = q.pathname === "/" ? "index.html" : `.${q.pathname}.html`;
+
     fs.readFile(filename, (err, data) => {
       if (err) {
-        res.writeHead(302, { Location: "/404.html" });
+        res.writeHead(302, { Location: "/404" });
         return res.end();
       }
       res.writeHead(200, { "Content-Type": "text/html" });
